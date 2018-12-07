4f63ce39832bbe23dd878eecedfb6763cd6d838f
develcrafter <nareumae@gmail.com>
2018-12-07 16:41:53 +0900

    Initialize, second: babel package is added

---
+++ README.md
@@ -0,0 +1,4 @@
+# GiTube
+
+### to practice making youtube clone
+To remake WeTube that is from Nomade Junior Full Stack Course that cloning YouTube.
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hi");
+import express from "express";
+
+const app = express();
+const PORT = 7104;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
+
+const handleHome = (req, res) => {
+  console.log("/");
+  res.send("This is Homepage!");
+};
+app.get("/", handleHome);
+
+const handleProfile = (req, res) => {
+  console.log("/profile");
+  res.send("Here is Profile page,");
+};
+app.get("/profile", handleProfile);
