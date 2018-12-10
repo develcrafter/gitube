b9dba0990dd86f899fd43ef29beed0ce5aabd882
develcrafter <nareumae@gmail.com>
2018-12-11 08:09:01 +0900

    CycleThree is installed middleWare

--- cycleThree/init.js
+++ cycleThree/init.js
@@ -1,10 +1,8 @@
-const express = require("express");
+// import express from "express";
+import app from "./app";
 
 const PORT = 7104;
 
-const app = express();
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
 app.listen(PORT, handleListening);
